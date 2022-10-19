7b2720d7a03b501e25552fe9e89e29f0771b733a
kryll <jose.amezcua.inf@gmail.com>
2022-10-19 19:01:30 +0200

    VPC

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 }
 
 variable "ssh_key_path" {}
+variable "availability_zone" {}
 
 # Recurso de clave SSH en AWS
 resource "aws_key_pair" "deployer" {
@@ -11,5 +12,44 @@ resource "aws_key_pair" "deployer" {
  public_key = file(var.ssh_key_path)
  tags = {
     Name = "jamezcua-ssh"
+ }
+}
+
+module "vpc" {
+ source = "terraform-aws-modules/vpc/aws"
+ name = "vpc-main"
+ cidr = "10.0.0.0/16"
+ azs = [var.availability_zone]
+ private_subnets = ["10.0.0.0/24", "10.0.1.0/24"]
+ public_subnets = ["10.0.100.0/24", "10.0.101.0/24"]
+ enable_dns_hostnames = true
+ enable_dns_support = true
+ enable_nat_gateway = false
+ enable_vpn_gateway = false
+ tags = { Terraform = "true", Environment = "dev" }
+}
+
+resource "aws_security_group" "allow_ssh" {
+ name = "allow_ssh"
+ description = "Allow SSH inbound traffic"
+ vpc_id = module.vpc.vpc_id
+
+ingress {
+ description = "SSH from VPC"
+ from_port = 22
+ to_port = 22
+ protocol = "tcp"
+ cidr_blocks = ["0.0.0.0/0"]
+ }
+
+egress {
+ from_port = 0
+ to_port = 0
+ protocol = "-1"
+ cidr_blocks = ["0.0.0.0/0"]
+ }
+
+ tags = {
+ Name = "allow_ssh"
  }
 }
