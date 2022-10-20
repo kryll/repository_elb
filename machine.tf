ed6056b07f21f0e028153c6174a4f03f27ef92b2
kryll <jose.amezcua.inf@gmail.com>
2022-10-20 17:26:16 +0200

    Nuew VM

--- machine.tf
+++ machine.tf
@@ -20,19 +20,36 @@ data "aws_ami" "rhel_8_5" {
   }
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
 resource "aws_instance" "web" {
   # ami a instalar
-  ami = data.aws_ami.rhel_8_5.id
+  ami = data.aws_ami.ubuntu.id
   # tipo de instancia
   instance_type = "t2.micro"
   # clave ssh asociada por defecto
   key_name = aws_key_pair.deployer.key_name
   # zona de disponibilidad
   availability_zone = var.availability_zone
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_http.id, aws_security_group.allow_https.id]
   subnet_id = element(module.vpc.public_subnets,1)
   tags = {
-    Name = "HelloWorld"
+    Name = "NewMachine_Ejercicio1"
   }
 }
 output "ip_instance" {
--- main.tf
+++ main.tf
@@ -54,3 +54,53 @@ egress {
  Name = "allow_ssh"
  }
 }
+
+resource "aws_security_group" "allow_https" {
+ name = "allow_https"
+ description = "Allow HTTPS inbound traffic"
+ vpc_id = module.vpc.vpc_id
+
+ingress {
+ description = "HTTPS from VPC"
+ from_port = 443
+ to_port = 443
+ protocol = "tcp"
+ cidr_blocks = ["0.0.0.0/0"]
+ }
+
+egress {
+ from_port = 0
+ to_port = 0
+ protocol = "-1"
+ cidr_blocks = ["0.0.0.0/0"]
+ }
+
+ tags = {
+ Name = "allow_https"
+ }
+}
+
+resource "aws_security_group" "allow_http" {
+ name = "allow_http"
+ description = "Allow HTTP inbound traffic"
+ vpc_id = module.vpc.vpc_id
+
+ingress {
+ description = "HTTP from VPC"
+ from_port = 80
+ to_port = 80
+ protocol = "tcp"
+ cidr_blocks = ["0.0.0.0/0"]
+ }
+
+egress {
+ from_port = 0
+ to_port = 0
+ protocol = "-1"
+ cidr_blocks = ["0.0.0.0/0"]
+ }
+
+ tags = {
+ Name = "allow_http"
+ }
+}
